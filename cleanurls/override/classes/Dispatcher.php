18e9ea99bea88327a0763a883ce6f8e17909ff0f
Lebioda <l.lebioda@gmail.com>
2013-10-25 22:58:54 +0200

    Possibility to use templates of links: domain.com/product domain.com/categories domain.com/manufacturer domain.com/supplier domain.com/cms

--- cleanurls/override/classes/Dispatcher.php
+++ cleanurls/override/classes/Dispatcher.php
@@ -100,80 +100,288 @@ class Dispatcher extends DispatcherCore
 	);
 	
 	/**
-	 * Load default routes group by languages
+	 * Check if $short_link is a Product Link
+	 *
+	 * @param string $short_link: requested url without '?' part and without '/' on begining
+	 * @return bool true: it's a link to product, false: it isn't
 	 */
-	protected function loadRoutes($id_shop = null)
+	public static function isProductLink($short_link)
 	{
-		$context = Context::getContext();
-		
-		// Load custom routes from modules
-		$modules_routes = Hook::exec('moduleRoutes', array('id_shop' => $id_shop), null, true, false);
-		if (is_array($modules_routes) && count($modules_routes))
-			foreach($modules_routes as $module_route)
-				foreach($module_route as $route => $route_details)
-					if (array_key_exists('controller', $route_details) && array_key_exists('rule', $route_details) 
-						&& array_key_exists('keywords', $route_details) && array_key_exists('params', $route_details))
-					{
-						if (!isset($this->default_routes[$route]))
-						$this->default_routes[$route] = array();
-						$this->default_routes[$route] = array_merge($this->default_routes[$route], $route_details);
-					}
-		
-		// Set default routes
-		foreach (Language::getLanguages() as $lang)
-			foreach ($this->default_routes as $id => $route)
-				$this->addRoute(
-					$id,
-					$route['rule'],
-					$route['controller'],
-					$lang['id_lang'],
-					$route['keywords'],
-					isset($route['params']) ? $route['params'] : array(),
-					$id_shop
-				);
-		
-		// Load the custom routes prior the defaults to avoid infinite loops
-		if ($this->use_routes)
+		$sql = 'SELECT `id_product`
+			FROM `'._DB_PREFIX_.'product_lang`
+			WHERE `link_rewrite` = \''.$short_link.'\' AND `id_lang` = '. Context::getContext()->language->id;
+
+		if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
 		{
-			// Get iso lang
-			$iso_lang = Tools::getValue('isolang');
-			$id_lang = $context->language->id;
-			if (!empty($iso_lang))
-				$id_lang = Language::getIdByIso($iso_lang);
-
-			// Load routes from meta table
-			$sql = 'SELECT m.page, ml.url_rewrite, ml.id_lang
-					FROM `'._DB_PREFIX_.'meta` m
-					LEFT JOIN `'._DB_PREFIX_.'meta_lang` ml ON (m.id_meta = ml.id_meta'.Shop::addSqlRestrictionOnLang('ml', $id_shop).')
-					ORDER BY LENGTH(ml.url_rewrite) DESC';
-			if ($results = Db::getInstance()->executeS($sql))
-				foreach ($results as $row)
-				{
-					if ($row['url_rewrite'])
-						$this->addRoute($row['page'], $row['url_rewrite'], $row['page'], $row['id_lang'], array(), array(), $id_shop);
-				}
-
-			// Set default empty route if no empty route (that's weird I know)
-			if (!$this->empty_route)
-				$this->empty_route = array(
-					'routeID' =>	'index',
-					'rule' =>		'',
-					'controller' =>	'index',
-				);
-
-			// Load custom routes
-			foreach ($this->default_routes as $route_id => $route_data)
-				if ($custom_route = Configuration::get('PS_ROUTE_'.$route_id, null, null, $id_shop))
-					foreach (Language::getLanguages() as $lang)
-						$this->addRoute(
-							$route_id,
-							$custom_route,
-							$route_data['controller'],
-							$lang['id_lang'],
-							$route_data['keywords'],
-							isset($route_data['params']) ? $route_data['params'] : array(),
-							$id_shop
-						);
+			$sql .= ' AND `id_shop` = '.(int)Shop::getContextShopID();
 		}
+
+		$id_product = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+			
+		return ($id_product > 0) ? true : false;
+	}
+	
+	/**
+	 * Check if $short_link is a Category Link
+	 *
+	 * @param string $short_link: requested url without '?' part and without '/' on begining
+	 * @return bool true: it's a link to category, false: it isn't
+	 */
+	public static function isCategoryLink($short_link)
+	{
+		// check if parent categories
+		$categories = explode("/", $short_link);
+		
+		$sql = 'SELECT `id_category` FROM `'._DB_PREFIX_.'category_lang`
+				WHERE `link_rewrite` = \''.$categories[0].'\' AND `id_lang` = '. Context::getContext()->language->id;
+
+		if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
+		{
+			$sql .= ' AND `id_shop` = '.(int)Shop::getContextShopID();
+		}
+		
+		$id_category = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+					
+		return ($id_category > 0) ? true : false;
+	}
+
+	/**
+	 * Check if $short_link is a Cms Link
+	 *
+	 * @param string $short_link: requested url without '?' part and without '/' on begining
+	 * @return bool true: it's a link to cms page, false: it isn't
+	 */
+	public static function isCmsLink($short_link)
+	{
+		$sql = 'SELECT l.`id_cms`
+			FROM `'._DB_PREFIX_.'cms_lang` l
+			LEFT JOIN `'._DB_PREFIX_.'cms_shop` s ON (l.`id_cms` = s.`id_cms`)
+			WHERE l.`link_rewrite` = \''.$short_link.'\'';
+
+		if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
+		{
+			$sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
+		}
+
+		$id_cms = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+					
+		return ($id_cms > 0) ? true : false;
+	}
+	
+	/**
+	 * Check if $short_link is a Manufacturer Link
+	 *
+	 * @param string $short_link: requested url without '?' part and without '/' on begining
+	 * @return bool true: it's a link to manufacturer, false: it isn't
+	 */
+	public static function isManufacturerLink($short_link)
+	{
+		$name_manufacturer = str_replace('-', '%', $short_link);
+
+		$sql = 'SELECT m.`id_manufacturer`
+			FROM `'._DB_PREFIX_.'manufacturer` m
+			LEFT JOIN `'._DB_PREFIX_.'manufacturer_shop` s ON (m.`id_manufacturer` = s.`id_manufacturer`)
+			WHERE m.`name` LIKE \''.$name_manufacturer.'\'';
+	
+		if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
+		{
+			$sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
+		}
+
+		$id_manufacturer = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+					
+		return ($id_manufacturer > 0) ? true : false;
+	}
+
+	/**
+	 * Check if $short_link is a Supplier Link
+	 *
+	 * @param string $short_link: requested url without '?' part and without '/' on begining
+	 * @return bool true: it's a link to supplier, false: it isn't
+	 */
+	public static function isSupplierLink($short_link)
+	{
+		$name_supplier = str_replace('-', '%', $short_link);
+
+		$sql = 'SELECT sp.`id_supplier`
+			FROM `'._DB_PREFIX_.'supplier` sp
+			LEFT JOIN `'._DB_PREFIX_.'supplier_shop` s ON (sp.`id_supplier` = s.`id_supplier`)
+			WHERE sp.`name` LIKE \''.$name_supplier.'\'';
+
+		if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
+		{
+			$sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
+		}
+
+		$id_supplier = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+					
+		return ($id_supplier > 0) ? true : false;
+	}
+
+	/**
+	 * Retrieve the controller from url or request uri if routes are activated
+	 *
+	 * @return string
+	 */
+	public function getController($id_shop = null)
+	{	
+		if (defined('_PS_ADMIN_DIR_'))
+			$_GET['controllerUri'] = Tools::getvalue('controller');		
+		if ($this->controller)
+		{
+			$_GET['controller'] = $this->controller;
+			return $this->controller;
+		}
+
+		if ($id_shop === null)
+			$id_shop = (int)Context::getContext()->shop->id;
+
+		$controller = Tools::getValue('controller');
+	
+		if (isset($controller) && is_string($controller) && preg_match('/^([0-9a-z_-]+)\?(.*)=(.*)$/Ui', $controller, $m))
+		{
+			$controller = $m[1];
+			if (isset($_GET['controller']))
+				$_GET[$m[2]] = $m[3];
+			else if (isset($_POST['controller']))
+				$_POST[$m[2]] = $m[3];
+		}
+
+		if (!Validate::isControllerName($controller))
+			$controller = false;
+	
+		// Use routes ? (for url rewriting)
+		if ($this->use_routes && !$controller && !defined('_PS_ADMIN_DIR_'))
+		{
+			if (!$this->request_uri)
+				return strtolower($this->controller_not_found);
+			$controller = $this->controller_not_found;
+			
+			// If the request_uri matches a static file, then there is no need to check the routes, we keep "controller_not_found" (a static file should not go through the dispatcher) 
+			if (!preg_match('/\.(gif|jpe?g|png|css|js|ico)$/i', $this->request_uri))
+			{
+				// Add empty route as last route to prevent this greedy regexp to match request uri before right time
+				if ($this->empty_route)
+					$this->addRoute($this->empty_route['routeID'], $this->empty_route['rule'], $this->empty_route['controller'], Context::getContext()->language->id, array(), array(), $id_shop);
+
+				if (isset($this->routes[$id_shop][Context::getContext()->language->id]))
+				{
+//					$firephp = FirePHP::getInstance(true);
+					$findRoute = array();
+					
+					// check, whether request_uri is template or not
+					foreach ($this->routes[$id_shop][Context::getContext()->language->id] as $route)
+					{
+//						$firephp->log($route['rule'], 'Rule');
+						
+						if (preg_match($route['regexp'], $this->request_uri, $m))
+						{
+//							$firephp->log('RegExp Pass!');
+							
+							$isTemplate = false;
+							
+							switch($route['controller'])
+							{
+								case 'supplier':
+								case 'manufacturer':
+									// these two can be processed in normal way and also as template
+									if(strpos($route['rule'], '{') !== false)
+									{
+										$isTemplate = true;
+									}
+									break;
+									
+								case 'cms':
+								case 'product':
+									$isTemplate = true;
+									break;
+								case 'category':
+									// category can be processed in two ways
+									if(strpos($route['rule'], 'selected_filters') === false)
+									{
+										$isTemplate = true;
+									}
+									break;
+							}
+							
+//							$firephp->log((int)$isTemplate, 'Template');
+							
+							if($isTemplate == false)
+							{
+								$findRoute = $route;
+								break;
+							}
+						}
+					}
+					
+					// if route is not found, we have to find rewrite link in database
+					if(empty($findRoute))
+					{
+						$req_url = substr($this->request_uri, 1); 		// remove '/' from begining
+						$req_url = explode("?", $req_url);				// remove all after '?'
+						$short_link = $req_url[0];
+						
+//						$firephp->log($short_link, 'Short Link');
+						
+						if(!Dispatcher::isProductLink($short_link))
+							if(!Dispatcher::isCategoryLink($short_link))
+								if(!Dispatcher::isCmsLink($short_link))
+									if(!Dispatcher::isManufacturerLink($short_link))
+										if(!Dispatcher::isSupplierLink($short_link))
+											{}
+										else
+											$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['supplier_rule'];
+									else
+										$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['manufacturer_rule'];
+								else
+									$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['cms_rule'];
+							else
+								$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['category_rule'];
+						else
+							$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['product_rule'];
+					}
+
+					if(!empty($findRoute))
+					{
+//						$firephp->log($findRoute['rule'], 'Find Route Template');
+						
+						if (preg_match($findRoute['regexp'], $this->request_uri, $m))
+						{
+							// Route found ! Now fill $_GET with parameters of uri
+							foreach ($m as $k => $v)
+								if (!is_numeric($k))
+									$_GET[$k] = $v;
+		
+							$controller = $findRoute['controller'] ? $findRoute['controller'] : $_GET['controller'];
+							if (!empty($findRoute['params']))
+								foreach ($findRoute['params'] as $k => $v)
+									$_GET[$k] = $v;
+		
+							// A patch for module friendly urls
+							if (preg_match('#module-([a-z0-9_-]+)-([a-z0-9]+)$#i', $controller, $m))
+							{
+								$_GET['module'] = $m[1];
+								$_GET['fc'] = 'module';
+								$controller = $m[2];
+							}
+		
+							if (isset($_GET['fc']) && $_GET['fc'] == 'module')
+								$this->front_controller = self::FC_MODULE;
+						}
+					}
+				}
+			}
+			
+			if ($controller == 'index' || $this->request_uri == '/index.php') 
+				$controller = $this->default_controller;
+			$this->controller = $controller;
+		}
+		// Default mode, take controller from url
+		else
+			$this->controller = $controller;
+
+		$this->controller = str_replace('-', '', $this->controller);
+		$_GET['controller'] = $this->controller;
+		return $this->controller;
 	}
 }

629d3d04f38375352adfa5d9756279db0db55a7b
drAlberT <albert@faktiva.com>
2015-02-26 20:14:36 +0100

    Revert "merge changes by wayann"

--- cleanurls/override/classes/Dispatcher.php
+++ cleanurls/override/classes/Dispatcher.php
@@ -7,47 +7,47 @@ class Dispatcher extends DispatcherCore
 	public $default_routes = array(
 		'supplier_rule' => array(
 			'controller' =>	'supplier',
-			'rule' =>		'supplier/{rewrite}/.html',
+			'rule' =>		'supplier/{rewrite}/',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'supplier_rewrite'),
+				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'supplier_rewrite'),
 				'meta_keywords' =>	array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 				'meta_title' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 			),
 		),
 		'manufacturer_rule' => array(
 			'controller' =>	'manufacturer',
-			'rule' =>		'manufacturer/{rewrite}/.html',
+			'rule' =>		'manufacturer/{rewrite}/',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'manufacturer_rewrite'),
+				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'manufacturer_rewrite'),
 				'meta_keywords' =>	array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 				'meta_title' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 			),
 		),
 		'cms_rule' => array(
 			'controller' =>	'cms',
-			'rule' =>		'page/{rewrite}.html',
+			'rule' =>		'info/{rewrite}',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'cms_rewrite'),
+				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'cms_rewrite'),
 				'meta_keywords' =>	array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 				'meta_title' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 			),
 		),
 		'cms_category_rule' => array(
 			'controller' =>	'cms',
-			'rule' =>		'page/{rewrite}/.html',
+			'rule' =>		'info/{rewrite}/',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'cms_category_rewrite'),
+				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'cms_category_rewrite'),
 				'meta_keywords' =>	array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 				'meta_title' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 			),
 		),
 		'module' => array(
 			'controller' =>	null,
-			'rule' =>		'module/{module}{/:controller}.html',
+			'rule' =>		'module/{module}{/:controller}',
 			'keywords' => array(
 				'module' =>			array('regexp' => '[_a-zA-Z0-9_-]+', 'param' => 'module'),
 				'controller' =>		array('regexp' => '[_a-zA-Z0-9_-]+', 'param' => 'controller'),
@@ -61,7 +61,7 @@ class Dispatcher extends DispatcherCore
 			'rule' =>		'{category:/}{rewrite}.html',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'product_rewrite'),
+				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'product_rewrite'),
 				'ean13' =>			array('regexp' => '[0-9\pL]*'),
 				'category' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 				'categories' =>		array('regexp' => '[/_a-zA-Z0-9-\pL]*'),
@@ -76,7 +76,7 @@ class Dispatcher extends DispatcherCore
 		),
 		'layered_rule' => array(
 			'controller' =>	'category',
-			'rule' =>		'{rewrite}/filter/{selected_filters}',
+			'rule' =>		'{rewrite}/filter{selected_filters}',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
 				/* Selected filters is used by the module blocklayered */
@@ -88,43 +88,12 @@ class Dispatcher extends DispatcherCore
 		),
 		'category_rule' => array(
 			'controller' =>	'category',
-			'rule' =>		'{parent_categories:/}{rewrite}/.html',
+			'rule' =>		'{parent_categories:/}{rewrite}/',
 			'keywords' => array(
 				'id' =>				array('regexp' => '[0-9]+'),
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'category_rewrite'),
+				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'category_rewrite'),
 				'meta_keywords' =>	array('regexp' => '[_a-zA-Z0-9-\pL]*'),
 				'meta_title' =>		array('regexp' => '[_a-zA-Z0-9-\pL]*'),
-				'parent_categories' =>		array('regexp' => '[/_a-zA-Z0-9-\pL]*'),
-				),
-		),
-		// added smartblog rules
-        'smartblog_rule' => array(
-            'controller' => 'category',
-            'rule' => 'blog/',
-			'keywords' => array(),
-            'params' => array(
-                'fc' => 'module',
-                'module' => 'smartblog',
-            ),
-        ),
-        'smartblog_category_rule' => array(
-            'controller' => 'category',
-            'rule' =>        'blog/motif/{category}',
-            'keywords' => array(
-                'id_category' =>    array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'id_category'),
-                'category'       =>   array('regexp' => '[_a-zA-Z0-9-\pL]*', 'param' => 'category'),
-            ),
-            'params' => array(
-                'fc' => 'module',
-                'module' => 'smartblog',
-            ),
-        ),
-		'category_page_rule' => array(
-			'controller' =>	'category',
-			'rule' =>		'{parent_categories:/}{rewrite}/page/{p}',
-			'keywords' => array(
-				'rewrite' =>		array('regexp' => '[_a-zA-Z0-9\pL\pS-]*', 'param' => 'category_rewrite'),
-				'p' =>		array('regexp' => '[0-9]+', 'param' => 'p'),
 				'parent_categories' =>		array('regexp' => '[/_a-zA-Z0-9-\pL]*'),
 				),
 		),
@@ -265,70 +234,6 @@ class Dispatcher extends DispatcherCore
 		return ($id_supplier > 0) ? true : false;
 	}
 
-
-	/**
-	 *
-	 * @param string $route_id Name of the route (need to be uniq, a second route with same name will override the first)
-	 * @param string $rule Url rule
-	 * @param string $controller Controller to call if request uri match the rule
-	 * @param int $id_lang
- 	 * @param int $id_shop
-	 */
-	public function addRoute($route_id, $rule, $controller, $id_lang = null, array $keywords = array(), array $params = array(), $id_shop = null)
-	{
-		if (isset(Context::getContext()->language) && $id_lang === null)
-			$id_lang = (int)Context::getContext()->language->id;
-
-		if (isset(Context::getContext()->shop) && $id_shop === null)
-			$id_shop = (int)Context::getContext()->shop->id;
-
-		$regexp = preg_quote($rule, '#');
-		if ($keywords)
-		{
-			$transform_keywords = array();
-			preg_match_all('#\\\{(([^{}]*)\\\:)?('.implode('|', array_keys($keywords)).')(\\\:([^{}]*))?\\\}#', $regexp, $m);
-			for ($i = 0, $total = count($m[0]); $i < $total; $i++)
-			{
-				$prepend = $m[2][$i];
-				$keyword = $m[3][$i];
-				$append = $m[5][$i];
-				$transform_keywords[$keyword] = array(
-					'required' =>	isset($keywords[$keyword]['param']),
-					'prepend' =>	stripslashes($prepend),
-					'append' =>		stripslashes($append),
-				);
-
-				$prepend_regexp = $append_regexp = '';
-				if ($prepend || $append)
-				{
-					$prepend_regexp = '('.preg_quote($prepend);
-					$append_regexp = preg_quote($append).')?';
-				}
-
-				if (isset($keywords[$keyword]['param']))
-					$regexp = str_replace($m[0][$i], $prepend_regexp.'(?P<'.$keywords[$keyword]['param'].'>'.$keywords[$keyword]['regexp'].')'.$append_regexp, $regexp);
-				else
-					$regexp = str_replace($m[0][$i], $prepend_regexp.'('.$keywords[$keyword]['regexp'].')'.$append_regexp, $regexp);
-
-			}
-			$keywords = $transform_keywords;
-		}
-
-		$regexp = '#^/'.$regexp.'(\?.*)?$#u';
-		if (!isset($this->routes[$id_shop]))
-			$this->routes[$id_shop] = array();
-		if (!isset($this->routes[$id_shop][$id_lang]))
-			$this->routes[$id_shop][$id_lang] = array();
-
-		$this->routes[$id_shop][$id_lang][$route_id] = array(
-			'rule' =>		$rule,
-			'regexp' =>		$regexp,
-			'controller' =>	$controller,
-			'keywords' =>	$keywords,
-			'params' =>		$params,
-		);
-	}
-
 	/**
 	 * Retrieve the controller from url or request uri if routes are activated
 	 *
@@ -344,11 +249,11 @@ class Dispatcher extends DispatcherCore
 			return $this->controller;
 		}
 
-		if (isset(Context::getContext()->shop) && $id_shop === null)
+		if ($id_shop === null)
 			$id_shop = (int)Context::getContext()->shop->id;
 
 		$controller = Tools::getValue('controller');
-
+	
 		if (isset($controller) && is_string($controller) && preg_match('/^([0-9a-z_-]+)\?(.*)=(.*)$/Ui', $controller, $m))
 		{
 			$controller = $m[1];
@@ -367,6 +272,7 @@ class Dispatcher extends DispatcherCore
 			if (!$this->request_uri)
 				return strtolower($this->controller_not_found);
 			$controller = $this->controller_not_found;
+			
 			// If the request_uri matches a static file, then there is no need to check the routes, we keep "controller_not_found" (a static file should not go through the dispatcher) 
 			if (!preg_match('/\.(gif|jpe?g|png|css|js|ico)$/i', $this->request_uri))
 			{
@@ -423,30 +329,6 @@ class Dispatcher extends DispatcherCore
 							}
 						}
 					}
-
-					// hack to make smartblog category path work. Remember to deactivate html in the module configuration
-					if (null !== Configuration::get('smartmainblogurl')) {
-						$blog_url = Configuration::get('smartmainblogurl');
-						$blog_url = '/'.$blog_url.'/';
-					}
-					else {
-						// no smart blog we assume a generic blog path
-						$blog_url = '/blog/';
-					}
-					
-					if ($this->request_uri === $blog_url) {
-						
-						$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['smartblog_rule'];
-						
-					}
-					
-					if (strpos($this->request_uri,'/motif/') !== false ) {
-						$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['smartblog_category_rule'];
-					}
-					
-					if (strpos($this->request_uri,'/page/') !== false ) {
-						$findRoute = $this->routes[$id_shop][Context::getContext()->language->id]['category_page_rule'];
-					}
 					
 					// if route is not found, we have to find rewrite link in database
 					if(empty($findRoute))
@@ -478,12 +360,14 @@ class Dispatcher extends DispatcherCore
 					if(!empty($findRoute))
 					{
 //						$firephp->log($findRoute['rule'], 'Find Route Template');
+						
 						if (preg_match($findRoute['regexp'], $this->request_uri, $m))
 						{
 							// Route found ! Now fill $_GET with parameters of uri
 							foreach ($m as $k => $v)
 								if (!is_numeric($k))
 									$_GET[$k] = $v;
+		
 							$controller = $findRoute['controller'] ? $findRoute['controller'] : $_GET['controller'];
 							if (!empty($findRoute['params']))
 								foreach ($findRoute['params'] as $k => $v)

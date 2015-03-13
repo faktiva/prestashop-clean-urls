399e334e027cf07e60cb2cf2d9994b246ecb11be
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2015-03-13 12:19:09 +0100

    - fixed parse error on cleanurls.php - fixed PS coding standards adeherence - security fix: use pSQL() to sanitize user values - some code cleanup

--- override/controllers/front/CategoryController.php
+++ override/controllers/front/CategoryController.php
@@ -1,25 +1,35 @@
 <?php
+
 /**
- * @module cleanurls
- * @author ZiZuu.com
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * It is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ *  @author      ZiZuu.com <info@zizuu.com>
+ *  @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  @source      https://github.com/ZiZuu-store/PrestaShop_module-CleanURLs
  */
 
 class CategoryController extends CategoryControllerCore
 {
 	public function init()
 	{
-		if (Tools::getValue('category_rewrite'))
+		if ($category_rewrite = Tools::getValue('category_rewrite'))
 		{
-			$category_rewrite = Tools::getValue('category_rewrite');
-
 			$sql = 'SELECT `id_category` FROM `'._DB_PREFIX_.'category_lang`
-					WHERE `link_rewrite` = \''.$category_rewrite.'\' AND `id_lang` = '. Context::getContext()->language->id;
+					WHERE `link_rewrite` = \''.pSQL($category_rewrite).'\' AND `id_lang` = '.Context::getContext()->language->id;
 
 			if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
 				$sql .= ' AND `id_shop` = '.(int)Shop::getContextShopID();
-			
-			$id_category = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
 
+			$id_category = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
 			if ($id_category > 0)
 				$_GET['id_category'] = $id_category;
 		}

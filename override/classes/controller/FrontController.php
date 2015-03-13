edddbd50601d8437a1d6ce6b103eb07786d86375
drAlberT <albert@faktiva.com>
2015-03-13 12:19:09 +0100

    - fixed parse error on cleanurls.php - fixed PS coding standards adeherence - security fix: use pSQL() to sanitize user values - some code cleanup

--- override/classes/controller/FrontController.php
+++ override/classes/controller/FrontController.php
@@ -1,45 +1,44 @@
 <?php
-/*
-* 2007-2013 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Open Software License (OSL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2013 PrestaShop SA
-*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * It is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ *  @author      ZiZuu.com <info@zizuu.com>
+ *  @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  @source      https://github.com/ZiZuu-store/PrestaShop_module-CleanURLs
+ */
 
 class FrontController extends FrontControllerCore
 {
-        public function __construct()
+	public function __construct()
 	{
 		parent::__construct();
 	}
 
 	protected function canonicalRedirection($canonical_url = '')
 	{
-		$excluded_keys = array('product_rewrite', 'category_rewrite', 'manufacturer_rewrite', 'supplier_rewrite',
-		                       'cms_rewrite', 'cms_category_rewrite');
+		$excluded_keys = array(
+			'product_rewrite',
+			'category_rewrite',
+			'manufacturer_rewrite',
+			'supplier_rewrite',
+			'cms_rewrite',
+			'cms_category_rewrite',
+		);
 
+		// hack original behavior on cananocalRedirection: remove *_rewrite from _GET
 		$unfiltered_GET = $_GET;
 		$_GET = array_diff_key($_GET, array_flip($excluded_keys));
 		parent::canonicalRedirection($canonical_url);
 		$_GET = $unfiltered_GET;
 	}				       
 }
-

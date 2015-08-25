de86524d0eb9f2d11da5b880747427bcd90d2bee
ZiZuu-store <info@ZiZuu.com>
2015-08-25 15:46:33 +0200

    PSR2

--- override/controllers/front/SupplierController.php
+++ override/controllers/front/SupplierController.php
@@ -19,25 +19,26 @@
 
 class SupplierController extends SupplierControllerCore
 {
-	public function init()
-	{
-		if ($supplier_rewrite = Tools::getValue('supplier_rewrite'))
-		{
-			$supplier_rewrite = str_replace('-', '%', $supplier_rewrite);
+    public function init()
+    {
+        if ($supplier_rewrite = Tools::getValue('supplier_rewrite')) {
+            $supplier_rewrite = str_replace('-', '%', $supplier_rewrite);
 
-			$sql = 'SELECT sp.`id_supplier`
+            $sql = 'SELECT sp.`id_supplier`
 				FROM `'._DB_PREFIX_.'supplier` sp
 				LEFT JOIN `'._DB_PREFIX_.'supplier_shop` s ON (sp.`id_supplier` = s.`id_supplier`)
 				WHERE sp.`name` LIKE \''.pSQL($supplier_rewrite).'\'';
 
-			if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
-				$sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
+            if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP) {
+                $sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
+            }
 
-			$id_supplier = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
-			if($id_supplier > 0)
-				$_GET['id_supplier'] = $id_supplier;
-		}
+            $id_supplier = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+            if ($id_supplier > 0) {
+                $_GET['id_supplier'] = $id_supplier;
+            }
+        }
 
-		parent::init();
-	}
+        parent::init();
+    }
 }

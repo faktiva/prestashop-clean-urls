c49af5767a7fbba46aa87ce9147c6db7252354c1
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2015-09-09 14:31:45 +0200

    code cleanup on front controllers

--- override/controllers/front/ManufacturerController.php
+++ override/controllers/front/ManufacturerController.php
@@ -19,13 +19,10 @@ class ManufacturerController extends ManufacturerControllerCore
     public function init()
     {
         if ($manufacturer_rewrite = Tools::getValue('manufacturer_rewrite')) {
-            $manufacturer_rewrite = str_replace('-', '%', $manufacturer_rewrite);
-
             $sql = 'SELECT m.`id_manufacturer`
                 FROM `'._DB_PREFIX_.'manufacturer` m
                 LEFT JOIN `'._DB_PREFIX_.'manufacturer_shop` s ON (m.`id_manufacturer` = s.`id_manufacturer`)
-                WHERE m.`name` LIKE \''.pSQL($manufacturer_rewrite).'\'';
-
+                WHERE m.`name` LIKE \''.pSQL(str_replace('-', '%', $manufacturer_rewrite)).'\'';
             if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP) {
                 $sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
             }
@@ -33,7 +30,6 @@ class ManufacturerController extends ManufacturerControllerCore
             $id_manufacturer = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
             if ($id_manufacturer > 0) {
                 $_GET['id_manufacturer'] = $id_manufacturer;
-                $_GET['noredirect'] = 1;
             }
         }
 

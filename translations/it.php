368a071bf1d71e39fb2a2eabbd69764dc7e49374
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2015-09-18 13:51:41 +0200

    fixed translations mechanism

---
+++ translations/it.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_66711d755755f94e8f48d9cb858290c8'] = 'ZiZuu Clean URLs';
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_4abf1025eb40d230f80b0470e5c60696'] = 'Questo modulo permette di rimuovere gli ID dalle URL. Utilizza gli \"override\".';
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_3c3419c3ec1896fbfd5c37549eda9144'] = 'Sicuro di voler rimuovere il modulo \"ZiZuu Clean URLs\"?';
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_851645568061b03aeeb2b4c2af435a5e'] = 'Dovresti eliminare queste URL duplicate:';
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_c961f5d807b8ae34a9a4a825606f953b'] = 'Lingua:';
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_63de125555fddc0e009f92ec0e9e64e4'] = 'Negozio:';
+$_MODULE['<{zzcleanurls}prestashop>zzcleanurls_dae2931f3df26903e6ddbb123b751344'] = 'Perfetto. Non ci sono URL duplicate.';

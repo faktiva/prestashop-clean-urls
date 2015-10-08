69213ea1e76ff3014513d085bda2311b852aae04
Antony Maker <amaker@faktiva.com>
2015-10-08 14:35:50 +0200

    ...

--- tests/setup/prestashop.sh
+++ tests/setup/prestashop.sh
@@ -10,7 +10,7 @@ sudo git clone --single-branch --branch ${PS_VERSION}  https://github.com/Presta
 # install it
 echo "php ${PS_ROOT}install-dev/index_cli.php --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name=prestashop.test --password=123456789"
 
-php ${PS_ROOT}install-dev/index_cli.php -- --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name=prestashop.test --password=123456789
+php ${PS_ROOT}install-dev/index_cli.php --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name=prestashop.test --password=123456789
 
 sleep 30
 

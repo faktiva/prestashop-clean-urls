f3a4d488ab9e445ff7d20c4aa5af18fe3b02bfbc
Antony Maker <amaker@faktiva.com>
2015-10-08 12:41:58 +0200

    remove constants, replace with protected vars

--- tests/functional/PrestashopHttpTest.php
+++ tests/functional/PrestashopHttpTest.php
@@ -2,11 +2,19 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-define('SAUCE_HOST', sprintf('%s:%s@ondemand.saucelabs.com', getenv('SAUCE_USERNAME'), getenv('SAUCE_ACCESS_KEY')));
-define('BASE_URL', getenv('TEST_PROTO').'://'.getenv('TEST_HOST').getenv('TEST_BASE_URI'));
-
 class PrestashopHttpTest extends Sauce\Sausage\WebDriverTestCase
 {
+    protected $sauce_host;
+    protected $base_url;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->sauce_host = sprintf('%s:%s@ondemand.saucelabs.com', getenv('SAUCE_USERNAME'), getenv('SAUCE_ACCESS_KEY'));
+        $this->base_url = sprintf('%s://%s%s', getenv('TEST_PROTO'), getenv('TEST_HOST'), getenv('TEST_BASE_URI'));
+    }
+
     public static $browsers = array(
         array(
             'browserName' => 'firefox',

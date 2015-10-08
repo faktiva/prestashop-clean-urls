5175711cee2a89699adff9d9ecca4c84b6b565a1
Antony Maker <amaker@faktiva.com>
2015-10-08 13:25:13 +0200

    ....

--- tests/functional/PrestashopHttpTest.php
+++ tests/functional/PrestashopHttpTest.php
@@ -4,32 +4,35 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 class PrestashopHttpTest extends Sauce\Sausage\WebDriverTestCase
 {
-    protected static $sauce_host;
-    protected static $base_url;
+    protected $base_url;
 
     public function __construct()
     {
         parent::__construct();
-
-        self::$sauce_host = sprintf('%s:%s@ondemand.saucelabs.com', getenv('SAUCE_USERNAME'), getenv('SAUCE_ACCESS_KEY'));
-        self::$base_url = sprintf('%s://%s%s', getenv('TEST_PROTO'), getenv('TEST_HOST'), getenv('TEST_BASE_DIR'));
+        $this->base_url = sprintf('%s://%s%s', getenv('TEST_PROTO'), getenv('TEST_HOST'), getenv('TEST_BASE_DIR'));
     }
 
     public static $browsers = array(
         array(
             'browserName' => 'firefox',
-            'host' => self:$sauce_host,
             'port' => 80,
             'desiredCapabilities' => array(
                 'version' => '15',
-                'platform' => 'Windows 2012',
+                'platform' => 'VISTA',
+            ),
+        ),
+        // run Chrome on Linux on Sauce
+        array(
+            'browserName' => 'chrome',
+            'desiredCapabilities' => array(
+                'platform' => 'Linux',
             ),
         ),
     );
 
     public function setUpPage()
     {
-        $this->url(self::$base_url);
+        $this->url($this->base_url);
     }
 
     public function testTitle()

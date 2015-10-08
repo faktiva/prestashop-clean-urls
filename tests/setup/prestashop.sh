903e64019d127dbed825b9841fc3eadd79eb8f15
Antony Maker <amaker@faktiva.com>
2015-10-08 14:30:37 +0200

    ...

--- tests/setup/prestashop.sh
+++ tests/setup/prestashop.sh
@@ -7,11 +7,11 @@ export PS_ROOT="${TEST_DOC_ROOT}${TEST_BASE_DIR}"
 # clone the right Prestashop version
 sudo git clone --single-branch --branch ${PS_VERSION}  https://github.com/PrestaShop/PrestaShop.git ${PS_ROOT}
 
-which php
-
 # install it
 echo "php ${PS_ROOT}install-dev/index_cli.php --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name=prestashop.test --password=123456789"
 
-php -l ${PS_ROOT}install-dev/index_cli.php
-command php ${PS_ROOT}install-dev/index_cli.php -- --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name=prestashop.test --password=123456789
+php ${PS_ROOT}install-dev/index_cli.php -- --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name=prestashop.test --password=123456789
 
+sleep 30
+
+echo PS DONE

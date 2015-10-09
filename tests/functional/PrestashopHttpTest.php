1023233777155b762f42d592ec6c2dff632d474e
Antony Maker <amaker@faktiva.com>
2015-10-09 10:39:52 +0200

    set Sauce tunnel identifier

--- tests/functional/PrestashopHttpTest.php
+++ tests/functional/PrestashopHttpTest.php
@@ -5,25 +5,27 @@ require_once __DIR__.'/../../vendor/autoload.php';
 class PrestashopHttpTest extends Sauce\Sausage\WebDriverTestCase
 {
     protected $base_url;
+    protected $job_id;
 
     public function __construct()
     {
         parent::__construct();
         $this->base_url = sprintf('%s://%s%s', getenv('TEST_PROTO'), getenv('TEST_HOST'), getenv('TEST_BASE_DIR'));
+        $this->job_id = getenv('TRAVIS_JOB_NUMBER');
     }
 
     public static $browsers = array(
         array(
-            'browserName' => 'firefox',
+            'browserName' => 'chrome',
             'desiredCapabilities' => array(
-                'version' => '15',
+                'tunnel-identifier' => $this->job_id,
                 'platform' => 'VISTA',
             ),
         ),
-        // run Chrome on Linux on Sauce
         array(
             'browserName' => 'chrome',
             'desiredCapabilities' => array(
+                'tunnel-identifier' => $this->job_id,
                 'platform' => 'Linux',
             ),
         ),

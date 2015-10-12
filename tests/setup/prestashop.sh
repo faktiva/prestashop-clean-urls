1fc12da0f64ba4c6746a0d3b36aa7f6ba35c6a53
Antony Maker <amaker@faktiva.com>
2015-10-12 11:09:53 +0200

    Update prestashop.sh

--- tests/setup/prestashop.sh
+++ tests/setup/prestashop.sh
@@ -2,15 +2,11 @@
 
 echo "Install and setup Prestashop"
 
-if [ -d ${PS_ROOT}/.git ]
-then
-    cd ${PS_ROOT}
-    git checkout -f --detach --no-track ${PS_VERSION}
-    cd -
-else
-    git clone --single-branch --branch ${PS_VERSION} https://github.com/PrestaShop/PrestaShop.git ${PS_ROOT}
-fi
+#get it
+git clone --single-branch --branch ${PS_VERSION} https://github.com/PrestaShop/PrestaShop.git ${PS_ROOT}
 
+#install & config
 php ${PS_ROOT}install-dev/index_cli.php --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name='Test Shop' --password=123456789
 
-rsync -av --exclude '/.*' --exclude '/composer.*' --exclude '/tests' --exclude '/vendor' ${TRAVIS_BUILD_DIR}/ ${TEST_DOC_ROOT}/modules/zzcleanurls/
+#install our module
+rsync -av --exclude '/.*' --exclude '/composer.*' --exclude '/tests' --exclude '/vendor' ${TRAVIS_BUILD_DIR}/ ${PS_ROOT}/modules/zzcleanurls/

20785620fcae156d3809d8725af752eaa2405a25
Antony Maker <amaker@faktiva.com>
2015-10-12 16:37:23 +0200

    Update prestashop.sh

--- tests/setup/prestashop.sh
+++ tests/setup/prestashop.sh
@@ -5,6 +5,9 @@ echo "Install and setup Prestashop"
 #get it
 git clone --single-branch --branch ${PS_VERSION} https://github.com/PrestaShop/PrestaShop.git ${PS_ROOT}
 
+#create DB as old PS did not
+mysql -uroot -e 'CREATE database IF NOT EXISTS prestashop.test;'
+
 #install & config
 php ${PS_ROOT}install-dev/index_cli.php --language=en --country=us --domain=${TEST_HOST} --base_uri=${TEST_BASE_DIR} --db_name=prestashop.test --db_create=1 --name='Test Shop' --password=123456789
 

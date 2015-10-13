7d6d1827c1df821d582ac998afe183d6f58afc77
Antony Maker <amaker@faktiva.com>
2015-10-13 19:14:39 +0200

    try to login

--- tests/functional/PrestashopBackOfficeTest.php
+++ tests/functional/PrestashopBackOfficeTest.php
@@ -40,4 +40,46 @@ class PrestashopBackOfficeTest extends Sauce\Sausage\WebDriverTestCase
 
         $this->assertContains('Administration panel', $this->title());
     }
+
+    public function testLoginFormExists()
+    {
+        $this->url('/_admin/');
+
+        $email = $this->byName('email');
+        $password = $this->byName('password');
+        $submit = $this->byName('submitLogin');
+
+        $this->assertEquals('', $email->value());
+        $this->assertEquals('', $password->value());
+        $this->assertEquals('submit', $submit->type());
+    }
+
+    public function testSubmitToSelf()
+    {
+        $this->url('/_admin/');
+
+        // create a form object for reuse
+        $form = $this->byId('login_form');
+
+        // get the form action
+        $action = $form->attribute('action');
+
+        // check the action value
+        $this->assertEquals('#', $action);
+
+        // fill in the form field values
+        $this->byName('email')->value('test@example.com');
+        $this->byName('password')->value('0123456789');
+
+        // submit the form
+        $form->submit();
+
+        $this->timeouts()->waitForPageToLoad();
+
+        // check if form was posted
+        $success = $this->byCssSelector('body.ps_back-office')->text();
+
+        // check the value
+        $this->assertContains('Dashboard', $success);
+    }
 }

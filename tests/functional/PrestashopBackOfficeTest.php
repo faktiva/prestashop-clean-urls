c66838cae2e98bcd3dd75571c505373b7e152250
Antony Maker <amaker@faktiva.com>
2015-10-13 16:25:16 +0200

    Update and rename PrestashopHttpTest.php to PrestashopBackOfficeTest.php

--- tests/functional/PrestashopBackOfficeTest.php
+++ tests/functional/PrestashopBackOfficeTest.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
-class PrestashopHttpTest extends Sauce\Sausage\WebDriverTestCase
+class PrestashopBackOfficeTest extends Sauce\Sausage\WebDriverTestCase
 {
     protected $base_url;
 
@@ -31,6 +31,8 @@ class PrestashopHttpTest extends Sauce\Sausage\WebDriverTestCase
 
     public function testBackOfficeTitle()
     {
+        $this->timeouts()->implicitWait(10000);
+
         $this->url($this->base_url.'_admin/');
         $this->assertContains('Test Shop', $this->title());
     }

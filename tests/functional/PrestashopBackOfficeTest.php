ae3329db284a8cbd736ad9000d64dd81a1592e85
Antony Maker <amaker@faktiva.com>
2015-10-14 13:27:29 +0200

    Update PrestashopBackOfficeTest.php

--- tests/functional/PrestashopBackOfficeTest.php
+++ tests/functional/PrestashopBackOfficeTest.php
@@ -57,43 +57,30 @@ class PrestashopBackOfficeTest extends Sauce\Sausage\WebDriverTestCase
     {
         $this->url('/_admin/');
 
-        // create a form object for reuse
-        $form = $this->byId('login_form');
-
-        // get the form action
-        $action = $form->attribute('action');
-
-        // fill in the form field values
         $this->byName('email')->value('test@example.com');
         $this->byName('passwd')->value('0123456789');
+        $this->byId('login_form')->submit();
 
-        // submit the form
-        $form->submit();
-
-        // check if form was posted
-        $success = $this->byCssSelector('body')->text();
-
-        // check the value
-        $this->assertContains('Dashboard', $success);
+        $this->assertContains('Dashboard', $this->byCssSelector('body')->text());
     }
 
     public function testModuleInstall()
     {
-        $this->open("/_bo/");
-        $this->assertTrue($this->isElementPresent("id=login"));
-        $this->type("id=email", "test@example.com");
-        $this->type("id=passwd", "0123456789");
-        $this->click("name=submitLogin");
-        $this->waitForPageToLoad("30000");
+        $this->url('/_admin/');
+        $this->assertTrue($this->isElementPresent('id=login'));
+        $this->type('id=email', 'test@example.com');
+        $this->type('id=passwd', '0123456789');
+        $this->click('name=submitLogin');
+        $this->waitForPageToLoad(30000);
 
-        $this->click("//li[@id='subtab-AdminModules']/a");
-        $this->waitForPageToLoad("30000");
-        $this->assertTrue((bool)preg_match('/^Modules[\s\S]*$/', $this->getTitle()));
+        $this->click('//li[@id=\'subtab-AdminModules\']/a');
+        $this->waitForPageToLoad(30000);
+        $this->assertContains('Modules', $this->getTitle());
 
-        $this->click("xpath=(//a[contains(@data-module-name, 'zzcleanurls')])");
-        $this->click("id=proceed-install-anyway");
-        $this->waitForPageToLoad("30000");
+        $this->click('xpath=(//a[contains(@data-module-name, \'zzcleanurls\')])');
+        $this->click('id=proceed-install-anyway');
+        $this->waitForPageToLoad(30000);
         $this->assertTrue((bool)preg_match('/^Modules[\s\S]*$/', $this->getTitle()));
-        $this->assertTrue((bool)preg_match('/^[\s\S]*configure=zzcleanurls[\s\S]*$/', $this->getLocation()));
+        $this->assertContains('configure=zzcleanurls', $this->getLocation());
     }
 }

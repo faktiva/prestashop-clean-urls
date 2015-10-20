7d7730e0b3c98871bf9379ee5638f95e1d538e3a
Antony Maker <amaker@faktiva.com>
2015-10-20 14:45:50 +0200

    [skip ci]

--- tests/functional/BackOfficeTest.php
+++ tests/functional/BackOfficeTest.php
@@ -19,6 +19,7 @@ class BackOfficeTest extends Sauce\Sausage\WebDriverTestCase
                 'recordVideo' => false,
                 'captureHtml' => true,
                 'idleTimeout' => 90,
+                'commandTimeout', => 300,
             ),
         ),
     );

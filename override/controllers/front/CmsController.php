728485d71242c31399177479cbbb7e68bdd6a18e
ZiZuu-store <info@ZiZuu.com>
2015-11-13 11:33:20 +0100

    Fixed 404 on CMS category

--- override/controllers/front/CmsController.php
+++ override/controllers/front/CmsController.php
@@ -13,16 +13,18 @@ class CmsController extends CmsControllerCore
                 FROM `'._DB_PREFIX_.'cms_lang` l
                 LEFT JOIN `'._DB_PREFIX_.'cms_shop` s ON (l.`id_cms` = s.`id_cms`)
                 WHERE l.`link_rewrite` = \''.pSQL(str_replace('.html', '', $cms_rewrite)).'\''.$shop_sql;
+
             $id_cms = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
             if ($id_cms > 0) {
                 $_GET['id_cms'] = $id_cms;
             }
         } elseif ($cms_category_rewrite = Tools::getValue('cms_category_rewrite')) {
-            $sql = 'SELECT `id_cms_category`
-                FROM `'._DB_PREFIX_.'cms_category_lang`
+            $sql = 'SELECT l.`id_cms_category`
+                FROM `'._DB_PREFIX_.'cms_category_lang` l
                 LEFT JOIN `'._DB_PREFIX_.'cms_category_shop` s ON (l.`id_cms_category` = s.`id_cms_category`)
                 WHERE `link_rewrite` = \''.pSQL($cms_category_rewrite).'\''.$shop_sql;
-			$id_cms_category = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+
+            $id_cms_category = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
             if ($id_cms_category > 0) {
                 $_GET['id_cms_category'] = $id_cms_category;
             }

e340b7b267c113e7ca80ea88937dd85dfebac047
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2015-12-01 16:50:13 +0100

    Release 1.0.3
    - support for '.html' extension on every URL (close issue #25)

--- override/controllers/front/CategoryController.php
+++ override/controllers/front/CategoryController.php
@@ -6,7 +6,7 @@ class CategoryController extends CategoryControllerCore
     {
         if ($category_rewrite = Tools::getValue('category_rewrite')) {
             $sql = 'SELECT `id_category` FROM `'._DB_PREFIX_.'category_lang`
-                WHERE `link_rewrite` = \''.pSQL($category_rewrite).'\' AND `id_lang` = '.Context::getContext()->language->id;
+                WHERE `link_rewrite` = \''.pSQL(str_replace('.html', '', $category_rewrite)).'\' AND `id_lang` = '.Context::getContext()->language->id;
             if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP) {
                 $sql .= ' AND `id_shop` = '.(int)Shop::getContextShopID();
             }

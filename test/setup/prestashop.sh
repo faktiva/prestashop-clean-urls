34ce4f3b635830e28983abcd299cc0b80293f33f
Antony Maker <amaker@faktiva.com>
2016-02-17 13:39:28 +0100

    Update prestashop.sh

--- test/setup/prestashop.sh
+++ test/setup/prestashop.sh
@@ -35,6 +35,9 @@ mv ${PS_ROOT%%/}/admin ${PS_ROOT%%/}/_admin
 mv ${PS_ROOT%%/}/install ${PS_ROOT%%/}/_install
 touch ${PS_ROOT%%/}/.htaccess && chmod 666 ${PS_ROOT%%/}/.htaccess
 
+#remove "Merchant Expertise" module as it is noisy
+rm -rf ${PS_ROOT%%/}/modules/gamification/
+
 #XXX
 echo -e "\033[33mls -lha ${PS_ROOT}\033[0m"
 ls -lha ${PS_ROOT}

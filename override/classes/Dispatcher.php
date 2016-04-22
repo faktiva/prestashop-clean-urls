2fe207999ab7ae37ed8d0bfc0aeb9e48dff59c9c
mpatzekov <mpatzekov@gmail.com>
2016-04-22 18:54:30 +0300

    Update Dispatcher.php

--- override/classes/Dispatcher.php
+++ override/classes/Dispatcher.php
@@ -372,23 +372,24 @@ class Dispatcher extends DispatcherCore
      */
     private static function isCategoryLink($short_link, $route)
     {
+        $category = basename($short_link);
+        $cat = explode("/",$short_link);
         $short_link = preg_replace('#\.html?$#', '', '/'.$short_link);
         $regexp = preg_replace('!\\\.html?\\$#!', '$#', $route['regexp']);
-
         preg_match($regexp, $short_link, $kw);
         if (empty($kw['category_rewrite'])) {
             return false;
         }
+         $sql = 'SELECT `'._DB_PREFIX_.'category_lang`.`id_category` FROM `'._DB_PREFIX_.'category_lang` JOIN `'._DB_PREFIX_.'category` on `'._DB_PREFIX_.'category_lang`.`id_category` = `'._DB_PREFIX_.'category`.`id_category`
+            WHERE `link_rewrite` = \''.pSQL(basename($category, '.html')).'\' AND '
+                . '((SELECT `link_rewrite` FROM `'._DB_PREFIX_.'category_lang` WHERE `id_category`=`id_parent`) = \''.$cat[count($cat)-2].'\''
+                . ' OR (SELECT `link_rewrite` FROM `'._DB_PREFIX_.'category_lang` WHERE `id_category`=`id_parent`) = \'home\')'
+                . ' AND `id_lang` = '.(int)Context::getContext()->language->id;
 
-        $sql = 'SELECT `id_category`
-            FROM `'._DB_PREFIX_.'category_lang`
-            WHERE `link_rewrite` = \''.pSQL($kw['category_rewrite']).'\' AND `id_lang` = '.(int)Context::getContext()->language->id;
         if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP) {
             $sql .= ' AND `id_shop` = '.(int)Shop::getContextShopID();
         }
-
         $id_category = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
-
         return ($id_category > 0);
     }
 

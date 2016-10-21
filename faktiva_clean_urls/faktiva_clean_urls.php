f164118d87b7bebb679baaa0744f38f4a584fcba
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2016-10-21 14:33:52 +0200

    Migrate the module to the new Faktiva maintainer

--- faktiva_clean_urls/faktiva_clean_urls.php
+++ faktiva_clean_urls/faktiva_clean_urls.php
@@ -1,46 +1,53 @@
 <?php
 
 /*
- * This file is part of the zzCleanURLs module.
+ * This file is part of the "Prestashop Clean URLs" module.
  *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
+ * (c) Faktiva (http://faktiva.com)
+ *
+ * NOTICE OF LICENSE
+ * This source file is subject to the CC BY-SA 4.0 license that is
+ * available at the URL https://creativecommons.org/licenses/by-sa/4.0/
  *
  * DISCLAIMER
  * This code is provided as is without any warranty.
  * No promise of being safe or secure
  *
- * @author   ZiZuu.com <info@zizuu.com>
- * @source   https://github.com/ZiZuu-store/zzCleanURLs
+ * @author   AlberT <albert@faktiva.com>
+ * @license  https://creativecommons.org/licenses/by-sa/4.0/  CC-BY-SA-4.0
+ * @source   https://github.com/faktiva/prestashop-clean-urls
  */
 
 if (!defined('_PS_VERSION_')) {
     return;
 }
 
-if (is_readable(__DIR__.'/vendor/autoload.php')) {
-    require_once __DIR__.'/vendor/autoload.php';
-    Symfony\Component\Debug\Debug::enable();
+// Set true to enable debugging
+define('FKV_DEBUG', false);
+
+if (version_compare(phpversion(), '5.3.0', '>=')) { // Namespaces support is required
+    include_once __DIR__.'/tools/debug.php';
 }
 
-class zzCleanUrls extends Module
+
+class faktiva_clean_urls extends Module
 {
     public function __construct()
     {
-        $this->name = 'zzcleanurls';
+        $this->name = 'faktiva_clean_urls';
         $this->tab = 'seo';
-        $this->version = '1.0.5';
-        $this->author = 'ZiZuu Store';
+        $this->version = '1.1.0';
+        $this->author = 'Faktiva';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
 
         parent::__construct();
 
-        $this->displayName = $this->l('ZiZuu Clean URLs');
+        $this->displayName = $this->l('Faktiva Clean URLs');
         $this->description = $this->l('This override-Module allows you to remove URL ID\'s.');
 
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall "ZiZuu Clean URLs"?');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall "Faktiva Clean URLs" module?');
     }
 
     public function getContent()
---
+++ faktiva_clean_urls/override/classes/controller/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the "Prestashop Clean URLs" module.
+ *
+ * (c) Faktiva (http://faktiva.com)
+ *
+ * NOTICE OF LICENSE
+ * This source file is subject to the CC BY-SA 4.0 license that is
+ * available at the URL https://creativecommons.org/licenses/by-sa/4.0/
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ * @author   AlberT <albert@faktiva.com>
+ * @license  https://creativecommons.org/licenses/by-sa/4.0/  CC-BY-SA-4.0
+ * @source   https://github.com/faktiva/prestashop-clean-urls
+ */
+
+header('Expires: Fri, 31 Dec 1999 23:59:59 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+return;

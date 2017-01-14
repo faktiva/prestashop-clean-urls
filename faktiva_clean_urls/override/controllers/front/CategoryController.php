b0f51e7ef67ea68b7f1b3fd18d4d286723cdee03
Luca T <misthero@gmail.com>
2017-01-14 14:27:54 +0100

    Allow non unique category name on different URI
    https://github.com/faktiva/prestashop-clean-urls/issues/44

--- faktiva_clean_urls/override/controllers/front/CategoryController.php
+++ faktiva_clean_urls/override/controllers/front/CategoryController.php
@@ -22,19 +22,30 @@ class CategoryController extends CategoryControllerCore
 {
     public function init()
     {
-        if ($category_rewrite = Tools::getValue('category_rewrite')) {
-            $sql = 'SELECT `id_category` FROM `'._DB_PREFIX_.'category_lang`
-                WHERE `link_rewrite` = \''.pSQL(str_replace('.html', '', $category_rewrite)).'\' AND `id_lang` = '.Context::getContext()->language->id;
-            if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP) {
-                $sql .= ' AND `id_shop` = '.(int) Shop::getContextShopID();
-            }
+      if ($category_rewrite = Tools::getValue('category_rewrite')) {
+          
+          $_base_url = str_replace(Tools::getHttpHost(true), '', $this->context->link->getPageLink('index',true));
+          $uri = str_replace($_base_url, '', $_SERVER['REQUEST_URI']);
+          $tree = explode("/", trim($uri,'/'));
+		  //echo '<pre>Cat controller tree:'.print_r($tree,1).'</pre>';
 
-            $id_category = (int) Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
-            if ($id_category > 0) {
-                $_GET['id_category'] = $id_category;
-            }
-        }
+		  $categories = Dispatcher::getSimpleCategories((int)Context::getContext()->language->id);
+		  $id_category = 0;
+		  foreach ($categories as $candidate){
+				$cloneTree = $tree;
+				if ($candidate['link_rewrite'] == array_pop($cloneTree)){
+					if (empty($cloneTree) || $candidate['is_root_category'] ||  Dispatcher::checkParentsLoop($candidate, $categories, $cloneTree)){
+						$id_category = (int)$candidate['id_category'];
+						break;
+					}
+				}
+			}
 
-        parent::init();
+          if ($id_category > 0) {
+              $_GET['id_category'] = $id_category;
+          }
+      }
+
+      parent::init();
     }
 }

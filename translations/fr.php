c83b5c8c6a81a7750a38aa0bf00661904f6088e5
sengap <agnes@alightweb.fr>
2017-07-06 13:02:10 +0200

    Create fr.php
    French translation working on PS 1.7.x

---
+++ translations/fr.php
@@ -0,0 +1,20 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_597084203e7c2329251d9f6055e815d7'] = 'Faktiva Clean URLs';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_4abf1025eb40d230f80b0470e5c60696'] = 'Ce module avec override vous permet de supprimer les ID dans les URLs.';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_5bdbfeeabb411d69e80a67e53f7af3ab'] = 'Etes-vous sûr(e) de vouloir désinstaller \"Faktiva Clean URLs\" ?';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_8aa6176f15ae0b8ec0f7f7797bc129cb'] = 'Sur certaines versions, vous devez désactiver le cache, enregistrer, ouvrir la page d\'accueil de votre boutique, revenir et réactiver le cache :';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_c9d7eedc8be4380c02106619824b8449'] = 'Paramètres avancés';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_9446a98ad14416153cc4d45ab8b531bf'] = 'Performances';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_341be761f9f11e9b06dac0071fb8a5ed'] = 'Vider le cache';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_d0834fcec6337785ee749c8f5464f6f6'] = 'Paramètres de la boutique';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_acc42ebe7b6b25f7d6ea8618f514f79b'] = 'SEO & URLs';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_a3637ddc43eefbe21947f64210bae984'] = 'Désactiver les URL simplifiées';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_7e497263357c140cc3d2396dbfa8b2eb'] = 'Activer les URLs simplifiées';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_851645568061b03aeeb2b4c2af435a5e'] = 'Vous devez réparer les URLs dupliquées :';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_c961f5d807b8ae34a9a4a825606f953b'] = 'Langue :';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_63de125555fddc0e009f92ec0e9e64e4'] = 'Boutique :';
+$_MODULE['<{faktiva_cleanurls}prestashop>faktiva_cleanurls_dae2931f3df26903e6ddbb123b751344'] = 'Parfait, vous n\'avez aucune URL dupliquée.';
